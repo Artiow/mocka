a6cece5d87ae2aadd067976e1fb48586d8fb59c2
artiow <namednev.artem@gmail.com>
2021-09-18 18:40:21 +0300

    runner upgrade

--- mocka-backend/src/main/java/org/mocka/runner/ScriptRunner.java
+++ mocka-backend/src/main/java/org/mocka/runner/ScriptRunner.java
@@ -14,19 +14,24 @@ import java.io.InputStreamReader;
 @RequiredArgsConstructor
 public class ScriptRunner {
 
+    @Deprecated(forRemoval = true)
     private static final String ENTRYPOINT = "main";
 
     private final ScriptEngine engine;
     private final ScriptStorage storage;
     private final JSObjectMapper mapper;
 
-
+    @Deprecated(forRemoval = true)
     public synchronized Object invoke(Object arg) throws ScriptRunnerException {
+        return invoke(ENTRYPOINT, arg);
+    }
+
+    public synchronized Object invoke(String entrypoint, Object arg) throws ScriptRunnerException {
         try {
             try (var reader = new InputStreamReader(storage.getScript())) {
                 engine.eval(reader);
             }
-            return ((Invocable) engine).invokeFunction(ENTRYPOINT, mapper.map(arg));
+            return ((Invocable) engine).invokeFunction(entrypoint, mapper.map(arg));
         } catch (Exception e) {
             throw new ScriptRunnerException("Exception occurred while script running", e);
         }
