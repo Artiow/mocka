e0475b3a3c5447e883abec2f59ab0f28fad9ba21
Anton Savchenko <lapital36@gmail.com>
2021-09-25 09:52:11 +0300

    feat: base layout with en editor

---
+++ mocka-frontend/src/constants/tailwind-tokens.ts
@@ -0,0 +1,7 @@
+// Default Tailwindcss font settings
+export const enum TailwindFont {
+  Sans = 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", ' +
+    'Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+  Serif = 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+  Mono = 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+}
